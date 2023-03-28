8e58e6b432f90ab37d5a10030000e1e421c7ac4f
cvtcvt007@naver.com <cvtcvt007@naver.com>
2023-03-28 15:21:48 +0900

    difficult histogram

--- Project2/20230328_practice1.cpp
+++ Project2/20230328_practice1.cpp
@@ -8,6 +8,17 @@ int main() {
 	IplImage* dst = cvCreateImage(size, 8, 3);
 
 
+	int histo[256][3] = { 0 };	//3 * 256
+
+
+		for (int y = 0; y < size.height; y++)
+			for (int x = 0; x < size.width; x++)
+			{
+				CvScalar c = cvGet2D(src, y, x);
+				for (int k = 0; k < 3; k++)
+					histo[(int)c.val[k]][k]++;
+			}
+
 
 
 	cvShowImage("src", src);
